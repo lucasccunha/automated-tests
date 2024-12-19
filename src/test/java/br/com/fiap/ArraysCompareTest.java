60686fefefdbc5c051e4d17a266784e9db2da4e4
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-18 23:38:23 -0300

    Comparando Arrays em JUnit Tests

---
+++ src/test/java/br/com/fiap/ArraysCompareTest.java
@@ -0,0 +1,22 @@
+package br.com.fiap;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class ArraysCompareTest {
+
+    @Test
+    void test() {
+        int[] numbers = {25,8,21,32,3};
+        int[] expectedArray = {3,8,21,25,32};
+
+        Arrays.sort(numbers);
+
+        assertArrayEquals(numbers, expectedArray);
+    }
+
+}
+

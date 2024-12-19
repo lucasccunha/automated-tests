d99e278e2b7c10d9798036ea77e5eba8186a66ef
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-19 18:16:22 -0300

    Annotation @RepeatedTest

---
+++ src/test/java/br/com/fiap/DemoRepeatedTest.java
@@ -0,0 +1,44 @@
+package br.com.fiap;
+
+import br.com.fiap.math.SimpleMath;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class DemoRepeatedTest {
+
+    SimpleMath math;
+    @BeforeEach
+    void beforeEachMethod() {
+        math = new SimpleMath();
+        System.out.println("Running @BeforeEach method!");
+    }
+    //@Disabled("TODO: We need still work on it")
+    @RepeatedTest(value = 3, name = "{displayName}. Repetition "
+            + "{currentRepetition} of {totalRepetitions}!")
+    @DisplayName("Test Division by Zero")
+    void testDivision_When_FirstNumberIsDividedByZero_ShouldThrowArithmeticException(
+            RepetitionInfo repetitionInfo,
+            TestInfo testInfo
+    ) {
+
+        System.out.println("Repetition N - " + repetitionInfo.getCurrentRepetition() +
+                " of " + repetitionInfo.getTotalRepetitions());
+        System.out.println("Running " + testInfo.getTestMethod().get().getName());
+        //given
+        double firstNumber = 6.2D;
+        double secondNumber = 0D;
+
+        var expectedMessage = "Impossible to divide by zero!";
+
+        //when & then
+        ArithmeticException actual = assertThrows(ArithmeticException.class, () -> {
+            //when & then
+            math.division(firstNumber, secondNumber);
+        }, () -> "Division by zero should throw an ArithmeticException");
+
+        assertEquals(expectedMessage,actual.getMessage(), () -> "Unexpected exception message!");
+    }
+
+}

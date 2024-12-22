248093a32737fbc76f1a8fe0734161a7e7e4d5a2
Lucas Cunha <lucas71cunha@gmail.com>
2024-12-22 17:24:26 -0300

    Refatorando os Testes Unitários

--- src/test/java/br/com/fiap/PersonServiceTest.java
+++ src/test/java/br/com/fiap/PersonServiceTest.java
@@ -2,30 +2,53 @@ package br.com.fiap;
 
 import br.com.fiap.model.Person;
 import br.com.fiap.service.IPersonService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import br.com.fiap.service.PersonService;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonServiceTest {
 
-    @DisplayName("When Create a Person with Success Should Return a Person Object")
+    Person person;
+
+    @BeforeEach
+    void setup() {
+        person = new Person(
+                "John",
+                "Doe",
+                "john.doe@example.com",
+                "123 Main St, Anytown, USA",
+                "Masculino"
+        );
+    }
+
+    @DisplayName("Quando criar uma pessoa com sucesso, deve retornar um objeto Pessoa")
     @Test
-    void testCreatePerson_WhenSucess_ShouldReturnPersonObject() {
+    void testCreatePerson_QuandoSucesso_DeveRetornarObjetoPessoa() {
 
         // Given / Arrange
         IPersonService service = new PersonService();
-        Person person = new Person (
-                "Keith",
-                "Moon",
-                "teste@teste.com.br",
-                "Wembley - UK ",
-                "Male"
-        );
+
         // When / Act
         Person actual = service.createPerson(person);
         // Then / Assert
-        assertNotNull(actual,() -> "The createPerson() should not have returned null!");
+        assertNotNull(actual, () -> "O método createPerson() não deveria ter retornado null!");
     }
+
+    @DisplayName("Quando criar uma pessoa com sucesso, deve conter o primeiro nome no objeto retornado")
+    @Test
+    void testCreatePerson_QuandoSucesso_DeveConterPrimeiroNomeNoObjetoRetornado() {
+
+        // Given / Arrange
+        IPersonService service = new PersonService();
+
+        // When / Act
+        Person actual = service.createPerson(person);
+        // Then / Assert
+        assertNotNull(actual, () -> "O método createPerson() não deveria ter retornado null!");
+        assertEquals(person.getFirstName(), actual.getFirstName(), () -> "O primeiro nome do objeto retornado não corresponde ao esperado!");
+    }
+
 }
